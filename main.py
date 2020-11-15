c524ba2b7a02eece5d3bcb01f4431536bf274e32
Daniel Mendes Guedes <danieldf100@outlook.com>
2020-11-15 19:32:29 -0300

    up

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ import requests
 from bs4 import BeautifulSoup
 
 #Criando variavel que vai receber o conteudo do site apontado. Também já configuramos o encoding para UTF-8
-res = requests.get("https://kardecpedia.com/")
+res = requests.get("https://digitalinnovation.one/blog/")
 res.encoding = 'utf-8'
-# print(res)
+print(res)
 # Neste momento sugiro fazer um teste com o terminal utilizando chamando o arquivo: "python main.py" 
 # Se o resultado for "Response [200]" significa que a concexão foi estabelecida. Se o resulta for diferente
 # aconteceu erro na conexão que pode ser por causa das autorizações do site.
@@ -24,20 +24,25 @@ res.encoding = 'utf-8'
 
 # PAra Trazer todo o conteudo da pagina, vamos criar outa variavel e dar um "soup" para que o html leia toda a # # #pagina como objetos
 
-soup = BeautifulSoup(res.text, 'html.parser')
+#soup = BeautifulSoup(res.text, 'html.parser')
 # print(soup)
 # Apartir da variavél SOUP vamos fazer o scraping 
-books = soup.find_all(class_="col-md-2 col-sm-3 col-xs-3 item")
+#posts = soup.find_all(class_="posts")
 				
 
 # Deve-se executar o arquivo no terminal para ver se está funcionando.
 # O Próximo passo é colocar a variavel "all_books" em um Dicionário para depois transformar em JSON
+'''
+all_posts = []
+for post in posts:
+    info = post.find(class_='post-content')
+    title = info.h2.text
+    preview = info.p.text
+    author = info.find(class_= "post-author").text'''
 
-all_books = []
-for book in books:
-    info = book.find(class_='books')
+        
     #print(book.find('h2').text
-    print(info)
+    
 
 
 
